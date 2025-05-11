5de53afeb9d71f175c4f77464a96b72d3eb7d639
Tarun-A-19 <tarun192006@gmail.com>
2025-05-11 16:22:57 +0530

    Nand2tetris till Project-5

--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -8,3 +8,28 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+(BEGIN)
+	// Initialize product to 0.
+	@R2
+	M=0
+(LOOP)
+	// If R1 <= 0, terminate.
+	@R1
+	D=M
+	@END
+	D;JLE
+	// Add R0 to R2.
+	@R0
+	D=M
+	@R2
+	M=M+D
+	// Decerement R1.
+	@R1
+	M=M-1
+	// Loop.
+	@LOOP
+	0;JMP
+(END)
+	// Terminate.
+	@END
+	0;JMP
